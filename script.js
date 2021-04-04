475f4e064f8651b55de5b21818001c231fddb0e2
4i! aka $0rrY <abrakadabra21099@gmail.com>
2021-04-04 17:55:56 +0400

    Learn 19

---
+++ script.js
@@ -0,0 +1,21 @@
+function a() {
+    // do something
+    setTimeout( function() 
+    {
+        console.log('a')
+    }, 500);
+}
+
+function b() {
+    console.log('b');
+}
+
+a();
+b();
+
+function c(lang, cb) {
+    console.log(`Я учу ${lang}!`);
+    cb();
+}
+
+c('JS', function() {console.log('Done!')});
